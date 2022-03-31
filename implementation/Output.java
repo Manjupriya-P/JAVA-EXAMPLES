5ae376ccf40f971cc050421bcdc538c1a5963a5e
Manjupriya-P <91693921+Manjupriya-P@users.noreply.github.com>
2022-03-31 16:42:18 +0530

    Thread implementation

---
+++ implementation/Output.java
@@ -0,0 +1,14 @@
+package com.onebill.implementation;
+
+public class Output {
+
+	public static void main(String[] args) {
+        		Thread1 thread1 = new Thread1();
+        		Thread thread = new Thread(thread1);
+        		thread.start();
+        	
+
+		
+			}
+
+}
