9f20644a35629751f49c9b47d07e3997001db0f3
Manjupriya-P <91693921+Manjupriya-P@users.noreply.github.com>
2022-03-31 16:02:51 +0530

    Constructor

---
+++ constructer/Cons.java
@@ -0,0 +1,19 @@
+package com.construct.constructer;
+
+public class Cons {
+	int a=5;
+	static float b=10.5f;
+	Cons(){//class name
+		
+		System.out.println("i am manju");
+	}
+
+	public static void main(String[] args) {
+		Cons objname=new Cons();
+		
+	}
+
+}
+//to create obj classname objname=new classname
+//we use obj to acess the members of the class
+//obj holds all the members of the class
---
+++ constructer/Object.java
@@ -0,0 +1,14 @@
+package com.construct.constructer;
+
+public class Object { 
+
+	public static void main(String[] args) {
+		Cons objname= new Cons();
+		System.out.println(objname.a);
+		System.out.println(Cons.b);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
